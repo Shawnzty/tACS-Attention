6ec5a7181a5031099f19e62b77f273d0ca241c74
Tianyi Zheng <zheng@neuron.t.u-tokyo.ac.jp>
2023-03-20 23:54:40 +0900

    MNE-python

--- eeglab_tutorial/tutorial.m
+++ eeglab_tutorial/tutorial.m
@@ -1,3 +1,6 @@
-eegdata = rand(32, 256*100); % 32 channels of random activity (100 s sampled at 256 Hz).
-eegdata(33,[10:256:256*100]) = 1; % simulating a stimulus onset every second
-eegdata(33,[100:256:256*100]+round(rand*128)) = 2; % simulating reaction times about 500 ms after stimulus onsets
+% eegdata = rand(32, 256*100); % 32 channels of random activity (100 s sampled at 256 Hz).
+% eegdata(33,[10:256:256*100]) = 1; % simulating a stimulus onset every second
+% eegdata(33,[100:256:256*100]+round(rand*128)) = 2; % simulating reaction times about 500 ms after stimulus onsets
+
+chanlocs = struct('labels', { 'cz' 'c3' 'c4' 'pz' 'p3' 'p4' 'fz' 'f3' 'f4'});
+pop_chanedit( chanlocs );
